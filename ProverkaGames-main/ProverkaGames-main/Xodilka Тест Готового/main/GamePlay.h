29974c1783d3d375b9ab7ccc6170646f92445fce
KykaFlan <dillerty_pro@mail.ru>
2026-01-09 20:20:37 +0500

    Add files via upload

---
+++ ProverkaGames-main/ProverkaGames-main/Xodilka Тест Готового/main/GamePlay.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// Глобальные переменные
+extern int KolichestvoPlayers;
+extern int winnerPlayer;
+
+
+// Функции игры (не все, только те которые выхываются вне GamePlay.cpp)
+void StartGame();
+int GetWinner();
